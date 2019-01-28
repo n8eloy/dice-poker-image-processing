30167ebfa4a41ab7c002e80ef153d2a19b86d165
Vitor Fogaça <38466161+Troncio@users.noreply.github.com>
2019-01-27 23:32:12 -0200

    Adicionado determinação do tipo da mão

---
+++ src/Hand.m
@@ -0,0 +1,104 @@
+classdef Hand
+   properties
+      hand_type % tipo da mão (Pair, Two Pairs, Three-of-a-kind etc...)
+      hand_strength % força da mão (vetor que guarda o valor dos dados da mão
+                    % forem [2 2 4 4 5 1], o hand_type será Two Pairs a e a
+                    % hand_strength será [4 2], pois o par mais forte é o
+                    % de 4 e o segundo par mais forte é o de 2.
+                    % Essa variável será usada no caso de haverem duas mãos
+                    % hand_types iguais entre os jogadores, para determinar
+                    % o vencedor
+      extra_dice % extra_dice são os dados extra rolados, ou seja, que não
+                 % fazem parte da hand_type, ordenados por força.
+                 % Usando o exemplo anterior, extra_dice seria [5 1], 
+                 % Pode ser necessário verificar os extra_dice no caso de
+                 % até a hand_strength ser igual
+   end
+   methods
+      function obj = Hand(dice_count)
+          % Determinando qual foi a mão do jogador
+            obj.hand_type = '';
+            three = 0;
+            pairs = [0 0];
+            five_str = 0;
+            six_str = 0;      
+            i = 1;
+            for k=1:6
+
+               if dice_count(k) == 1 % para verificar five high straight ou six high straight
+                   obj.extra_dice(i) = k;
+                   i = i + 1;
+                   if k == 1
+                       five_str = 1;
+                   elseif k == 2
+                       if five_str == 0
+                            six_str = 1;
+                       end
+                   end
+               else
+                   if (k ~= 6)
+                        five_str = 0;
+                   end
+                   six_str = 0;
+
+                   if dice_count(k) == 5
+                        obj.hand_type = 'Five-of-a-Kind'; % five-of-a-kind
+                        obj.hand_strength = [k];
+                        obj.extra_dice = null;
+                        break;
+                   elseif dice_count(k) == 4
+                        obj.hand_type = 'Four-of-a-Kind'; % four-of-a-kind
+                        obj.hand_strength = [k];
+                   elseif dice_count(k) == 3
+                       three = k;
+                       if pairs > 0
+                           obj.hand_type = 'Full House'; % full house
+                           obj.hand_strength = [k, pairs(1)];
+                           break;
+                       end
+                   elseif dice_count(k) == 2
+                       if pairs(1) == 0
+                           pairs(1) = k;
+                       else
+                           pairs(2) = k;
+                       end      
+                       
+                       if three ~= 0
+                           obj.hand_type = 'Full House'; % full house
+                           obj.hand_strength = [three, pairs(1)];
+                           break;
+                       elseif pairs(1) ~= 0
+                           obj.hand_type = 'Two Pairs'; % two pairs
+                           obj.hand_strength = [pairs(1), pairs(2)];
+                       end
+                   end
+               end
+            end
+           
+            obj.extra_dice = sort(obj.extra_dice, 'descend'); % Ordenando os dados extra pela força
+            if pairs(2) ~= 0
+                obj.hand_strength = sort(obj.hand_strength, 'descend');
+            end
+            
+            if strcmp(obj.hand_type, '') % Se a mão ainda não foi determinada no loop
+                if six_str == 1 % Six High Straight
+                    obj.hand_type = 'Six High Straight';
+                    obj.hand_strength = null;
+                elseif five_str == 1 % Five High Straight
+                    obj.hand_type = 'Five High Straight';
+                    obj.hand_strength = null;
+                elseif three ~= 0 % Three-of-a-Kind
+                    obj.hand_type = 'Three-of-a-Kind';
+                    obj.hand_strength = [three];
+                elseif pairs(1) ~= 0 % Pair
+                    obj.hand_type = 'Pair';
+                    obj.hand_strength = [pairs(1)];
+                else % Nothing
+                    obj.hand_type = 'Nothing';
+                    obj.hand_strength = obj.extra_dice;
+                end 
+            end
+          
+      end
+   end
+end
--- src/dicepoker.m
+++ src/dicepoker.m
@@ -11,6 +11,7 @@ clc;
 % Carregando as imagens
 roll_img = imread('imgs/dice-rolls/dice-roll-test-1.png');
 result = [0 0 0 0 0];
+dice_count = [0 0 0 0 0 0];
 dice1 = imread('imgs/dice-values/dice-1.jpg');
 dice2 = imread('imgs/dice-values/dice-2.jpg');
 dice3 = imread('imgs/dice-values/dice-3.jpg');
@@ -20,7 +21,7 @@ dice6 = imread('imgs/dice-values/dice-6.jpg');
 
 % Mostrando a imagem original
 figure('Name','Roll (Original)'),set(gcf, 'Units','Normalized','OuterPosition',[0 0 1 1]),
-subplot(1,2,1),imshow(roll_img),title('Roll (Original)');
+imshow(roll_img),title('Roll (Original)');
 
 % Converte as imagens se necessário (mais de um canal de cores na imagem)
 [r1, c1, canais1] = size(roll_img);
@@ -36,15 +37,24 @@ bw_roll = imdilate(bw_roll, strel('sphere', 2));
 [rotulada1, qtd_regioes1] = bwlabel(bw_roll);
 
 % Regionprops
-props1 = regionprops(rotulada1, 'EulerNumber', 'Centroid');
+props1 = regionprops(rotulada1, 'EulerNumber', 'Centroid', 'BoundingBox');
 
 % Mostrando a imagem com erosão e número de Euler
-subplot(1,2,2), imshow(bw_roll), title('Roll'),
+figure('Name','Roll (bw)'),set(gcf, 'Units','Normalized','OuterPosition',[0 0 1 1]),
+imshow(bw_roll), title('Roll (bw)');
+
+for k=1:qtd_regioes1
+    thisBB = props1(k).BoundingBox;
+    rectangle('Position', [thisBB(1),thisBB(2),thisBB(3),thisBB(4)],...
+    'EdgeColor','r','LineWidth',2 )
+    text(thisBB(1,1) + 3, thisBB(1,2) - 8, num2str(abs(props1(k).EulerNumber) + 1), 'FontSize', 14, 'FontWeight' , 'Bold', 'Color', 'Red', 'HorizontalAlignment', 'Center');
+end
 
 % Criando figura para exibir a jogada e mostrar outras informações
 figure('Name','Result'),set(gcf, 'Units','Normalized','OuterPosition',[0 0 1 1]);
 for k=1:qtd_regioes1
     result(k) = abs(props1(k).EulerNumber) + 1; % vetor para armazenar valores dos d6 rolados
+    dice_count(result(k)) = dice_count(result(k)) + 1; % vetor para armazenar a contagem de cada valor rolado
     
     % If para exibir a jogada
     if result(k) == 1
@@ -61,3 +71,11 @@ for k=1:qtd_regioes1
         subplot(2,5,k+5),imshow(dice6),title('6');
     end
 end
+
+% Criando os objetos Hand
+jogador1 = Hand(dice_count);
+
+ax = subplot(2, 5, 2);
+text(0.5, 0.5, ['Tipo da Mão: ' jogador1.hand_type char(10) 'Força da mão: ' num2str(jogador1.hand_strength(1)) ', ' num2str(jogador1.hand_strength(2)) char(10) 'Dados extra: ' num2str(jogador1.extra_dice(1))],...
+    'FontSize', 32, 'FontWeight' , 'Bold', 'Color', 'Red');
+set ( ax, 'visible', 'off');
